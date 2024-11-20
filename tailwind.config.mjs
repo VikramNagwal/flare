d34cdd63a4548d6ede021add7b9fa946a7f2e085
Vikram Singh <nagwalanshu@gmail.com>
2024-11-20 20:43:21 +0530

    initial commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,23 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				monteserrat: ['Montserrat', 'sans-serif'],
+			},
+			screens: {
+				mdl: '1045px'
+			},
+			animation: {
+				float: 'float 3s infinite',
+			},
+			keyframes: {
+				float: {
+					'0%, 100%': { transform: 'translateY(0)', },
+					'50%': { transform: 'translateY(-5%)', },
+				},
+			}
+		},
 	},
 	plugins: [],
 }
